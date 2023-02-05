26cfa275f71b751c3ab73c0338c087405c00b219
1qwerdzp <120164884+1qwerdzp@users.noreply.github.com>
2023-02-05 17:30:40 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
-import turtle
+import turtle as t
 
 s=turtle.Screen()
 s.setup(650, 800)
 s.bgcolor("lightcyan")
-#s.tracer(0)
-t=turtle.Turtle()
+s.tracer(0)
 t.pensize(5)
 t.speed('fastest')
-t.color("black",#FFFFFF)
+t.color(#000000,#FFFFFF)
 t.penup()
 t.goto(0,0)
 t.pendown()
